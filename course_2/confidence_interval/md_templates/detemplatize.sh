ddd21f4e39e399f952bbbb24691193014e00a98e
David Tonhofer <ronerycoder@gluino.name>
2020-02-27 23:02:29 +0100

    Trying to get this right

--- course_2/confidence_interval/md_templates/detemplatize.sh
+++ course_2/confidence_interval/md_templates/detemplatize.sh
@@ -7,8 +7,14 @@ for file in description.md; do
    if [[ ! -f $template ]]; then
       echo "No file '$template' -- skipping" >&2
    else
-      sed 's/\$WHERE\$/https:\/\/raw.githubusercontent.com\/dtonhofer\/rstudio_coding\/master\/course_2/g' \
-      < "$template" > "../$file"
+      # Use tilde instead of "/" as separator
+      raw="https://raw.githubusercontent.com/dtonhofer/rstudio_coding/master"
+      blob="https://github.com/dtonhofer/rstudio_coding/blob/master"
+      cat "$template" | \
+        sed "s~%DIAGRAMS%~${raw}/course_2/confidence_interval/diagrams~g" | \
+        sed "s~%PLOTS%~${raw}/course_2/confidence_interval/plots~g" | \
+        sed "s~%CODE%~${blob}/course_2/confidence_interval/code~g" \
+      > "../$file"
    fi
 done
 
