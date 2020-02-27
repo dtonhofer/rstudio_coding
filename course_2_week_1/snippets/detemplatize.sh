36fb471a3165e44f1b044bde9abb93aede2834ca
David Tonhofer <ronerycoder@gluino.name>
2020-02-27 17:22:12 +0100

    Move gist to here

--- course_2_week_1/snippets/detemplatize.sh
+++ course_2_week_1/snippets/detemplatize.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-out=sample_stats_plot_ames.md
-in=${out}.template
+for file in plot_pdf_of_area.md sample_stats_plot_ames.md; do
+   template="${file}.template"
+   if [[ ! -f $template ]]; then
+      echo "No file '$template' -- skipping" >&2
+   else
+      sed 's/\$WHERE\$/https:\/\/raw.githubusercontent.com\/dtonhofer\/rstudio_coding\/master\/course_2_week_1\/plots/g' \
+      < "$template" > "$file"
+   fi
+done
 
-if [[ ! -f $in ]]; then
-   echo "No file '$in' -- exiting" >&2
-   exit 1
-fi
-
-sed 's/\$WHERE\$/https:\/\/raw.githubusercontent.com\/dtonhofer\/rstudio_coding\/master\/course_2_week_1\/plots/g' < "$in" > "$out"
 
 
 
